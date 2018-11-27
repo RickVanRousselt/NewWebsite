ea49d5077e329c0e189eaaf731eb781e9b5dcb02
Rick Van Rousselt <rick@rivaro.be>
2018-11-27 14:10:02 +0100

    Update

---
+++ WebApplication1/WebApplication1/Controllers/CallController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+using Twilio.TwiML;
+
+namespace WebApplication1.Controllers
+{
+    public class CallController
+    {
+        [System.Web.Http.HttpPost]
+        public TwiML Index()
+        {
+            var response = new VoiceResponse();
+            response.Say("Thank you for calling! Have a great day.");
+
+            return response;
+        }
+    }
+}
